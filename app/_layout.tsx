9529f855a47790f2afc78abff075a65dc25c3bb1
davidvanstory <d.vanstory01@gmail.com>
2025-06-23 18:07:32 -0700

    supabase credentials

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import 'react-native-url-polyfill/auto';
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
